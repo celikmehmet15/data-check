759656b6816273d33a015d9494777eeb6984cee4
Mehmet Celik <celikmehmet1507@gmail.com>
2026-02-09 23:51:08 +0300

    Solved the first challenge of the workintech :)

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
-
+"""Workintech data-check exercise."""
+import math
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    """Returns the area of the circle of given radius."""
+    if radius < 0:
+        return 0
+    return math.pi * (radius ** 2)
